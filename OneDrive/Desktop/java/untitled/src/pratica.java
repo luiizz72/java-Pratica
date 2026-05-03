335adb2b8497ed8c3426d91ae7ecb94aa66a47e2
luiizz72 <169167567+luiizz72@users.noreply.github.com>
2026-05-03 12:07:25 -0300

    pratica

---
+++ OneDrive/Desktop/java/untitled/src/pratica.java
@@ -0,0 +1,47 @@
+public class pratica {
+    public static void main(String[]args){
+
+        /*double a=5.2, b=5.6, c=-5.8;
+        System.out.println("Arredondando  5.2 = "+Math.ceil(a));
+        System.out.println("Arredondando  5.6 = "+Math.ceil(b));
+        System.out.println("Arredondando  -5.8 = "+Math.ceil(c));
+
+        double a=5.2, b=5.6, c=-5.8;
+        System.out.println("Arredondando2 5.2 = " +Math.floor(a));
+        System.out.println("Arredondando2 5.6 = " +Math.floor(b));
+        System.out.println("Arredondando2 -5.8= " +Math.floor(c));
+
+        int a=10, b=15;
+        double c=-5.9, d=-4.5;
+        System.out.println("O maior entre 10 e 15 é = "+Math.max(a,b));
+        System.out.println("O maior entre -5.9 e -4.5 = " +Math.max(c,d));
+        System.out.println("O maior entre 10 e -5.9 é = "+Math.max(a,c));
+
+        int a=10, b=15;
+        double c=-5.9, d=-4.5;
+        System.out.println("O menor entre 10 e 15 é = "+Math.min(a,b));
+        System.out.println("O menor entre -5.9 e -4.5 = " +Math.min(c,d));
+        System.out.println("O menor entre 10 e -5.9 é = "+Math.min(a,c));
+
+        double a=900, b=30.25;
+        System.out.println("A raiz quadrada de 900 é= "+Math.sqrt(a));
+        System.out.println("A raiz quadrada de 30.25 é= " +Math.sqrt(b));
+
+        double base=5.5, potencia=2;
+        System.out.println("5.5 elevado a 2 é = "+Math.pow(base,potencia));
+        System.out.println("25 elevado a 0.5 é = "+Math.pow(25,.5));
+        System.out.println("5678 elevado a 0 é = "+Math.pow(5678,0));
+
+        double  a= 5.5, b=2.5, c=1.8;
+        System.out.println("Arredondando  5.5 = "+Math.ceil(a));
+        System.out.println("Arredondando  2.5 = "+Math.ceil(b));
+        System.out.println("Arredondando  1.8 = "+Math.ceil(c));
+
+        double a=1, b=2.5;
+        System.out.println("A raiz quadrada de 1 é= "+Math.sqrt(a));
+        System.out.println("A raiz quadrada de 1 é= "+Math.sqrt(b));*/
+
+        int a=5, b=25;
+        System.out.println("O maior entre 5 e 25 é = "+Math.max(a,b));
+    }
+}
